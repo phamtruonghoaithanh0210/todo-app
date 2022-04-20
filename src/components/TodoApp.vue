c22f6e90134e22e1fedac8bedecbc26f68910ab5
phamtruonghoaithanh0210 <1851050129thanh@ou.edu.vn>
2022-04-21 03:45:09 +0700

    update 20/4

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,6 +1,41 @@
 <template>
-  <div class="hello">
-    hello World
+  <div class="container">
+    <h2 class="text-center mt-5">hello World</h2>
+
+    <!-- Input -->
+    <div class="d-flex">
+        <input v-model="task" type="text" placeholder="Enter Task" class="form-control">
+        <button @click="submitTask" class="btn btn-warning rounded-0">SUBMIT</button>
+    </div>
+
+
+    <!-- Task Table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task,index) in tasks" :key="index">
+          <td>{{task.name}}</td>
+          <td>{{task.status}}</td>
+          <td>
+            <div class="text-center" @click="editTask(index)">
+              <font-awesome-icon icon="fa-solid fa-pen" />
+            </div>
+          </td>
+          <td>
+            <div class="text-center" @click="deleteTask(index)">
+              <font-awesome-icon icon="fa-solid fa-trash" />
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,6 +44,50 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      task : '',
+      editedTask : null,
+      tasks : [
+        {
+          name: "Ngủ Dậy Lúc 9h.",
+          status: "to-do"
+        },
+        {
+          name: "Ăn Sáng",
+          status: "in-progress"
+        },
+      ]
+    }
+  },
+
+  methods:{
+    submitTask(){
+      if(this.task.length === 0) return;
+
+      if(this.editedTask === null){
+        this.tasks.push({
+          name: this.task,
+          status: 'to-do'
+        })
+      }else{
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null
+      }
+
+      this.task = ''
+    },
+
+    deleteTask(index){
+      this.tasks.splice(index,1)
+    },
+
+    editTask(index){
+      this.task = this.tasks[index].name
+      this.editedTask = index
+    }
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faPen, faTrash)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
